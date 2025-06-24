2c927316678839659426517a89822739b69b5dd1
ankitxcoding <ankitxcoding@gmail.com>
2025-06-24 18:49:30 +0530

    recursion

---
+++ recursionPractice.py
@@ -0,0 +1,141 @@
+def printNto1(n):
+    if n == 0:
+        return
+
+    print(n)
+    printNto1(n - 1)
+
+
+def print1toN(n):
+    if n == 0:
+        return
+
+    print1toN(n - 1)
+    print(n)
+
+
+def printEveryNumberTwiceExeptLargest(n):
+    if n == 0:
+        return
+
+    printEveryNumberTwiceExeptLargest(n - 1)
+    print(n)
+    printEveryNumberTwiceExeptLargest(n - 1)
+
+    # this will print every twice except the largest one, largest come into the middle.
+    # e.g. for n = 3, output will be 1 2 1 3 1 2 1
+
+
+def printNto1and1toN(n):
+    if n == 0:
+        return
+
+    print(n)
+    printNto1and1toN(n - 1)
+    print(n)
+
+
+def print1toNandNto1(n, x):
+    if x > n:
+        return
+
+    print(x)
+    print1toNandNto1(n, x + 1)
+    print(x)
+
+
+def printDecimalToBinary(n):
+    if n == 0:
+        return
+
+    printDecimalToBinary(n // 2)
+    print(n % 2, end="")
+
+
+def printStars(n, x):
+    if n == 0:
+        return
+
+    print("*" * x)
+    printStars(n - 1, x)
+
+
+def print1toNStars(n, x):
+    if n == 0:
+        return
+
+    print("*" * x)
+    print1toNStars(n - 1, x + 1)
+
+
+def printNto1Stars(n):
+    if n == 0:
+        return
+
+    print("*" * n)
+    printNto1Stars(n - 1)
+
+
+def printNto1LadderRow(n):
+    if n == 0:
+        return
+
+    print(str(n) * n)
+    printNto1LadderRow(n - 1)
+
+    # Output -
+    # 55555
+    # 4444
+    # 333
+    # 22
+    # 1
+
+
+def print1toNLadderRow(n):
+    if n == 0:
+        return
+
+    print1toNLadderRow(n - 1)
+    print(str(n) * n)
+
+    # Output -
+    # 1
+    # 22
+    # 333
+    # 4444
+    # 55555
+
+
+def printNto1LadderCol(n, x):
+    if n == 0:
+        return
+
+    print(str(x))
+    printNto1LadderCol(n - 1, x + 1)
+
+
+n = int(input())
+
+# printNto1(n)
+
+# print1toN(n)
+
+# printEveryNumberTwiceExeptLargest(n)
+
+# printNto1and1toN(n)
+
+# print1toNandNto1(n, 1)
+
+# printDecimalToBinary(n)
+
+# printStars(n, n)
+
+# print1toNStars(n, 1)
+
+# printNto1Stars(n)
+
+# printNto1LadderRow(n)
+
+# print1toNLadderRow(n)
+
+printNto1LadderCol(n, 1)
